2aea464f650067c4e259419a87ee2f3fa9c96b9d
Oladusheckc <oladusheckru@gmail.com>
2022-12-08 11:06:25 +0500

    kak eto delat

--- Labirint/Form1.Designer.cs
+++ Labirint/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
@@ -56,7 +59,9 @@
             "20",
             "50",
             "100",
-            "1000"});
+            "200",
+            "400",
+            "800"});
             this.comboBox1.Location = new System.Drawing.Point(116, 35);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(115, 23);
@@ -66,21 +71,59 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(0, 37);
+            this.groupBox1.Location = new System.Drawing.Point(3, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(243, 692);
+            this.groupBox1.Size = new System.Drawing.Size(243, 718);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Создание лабиринта";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Идеальный",
+            "Плетенный",
+            "Одномаршрутный",
+            "Рандомные проходы"});
+            this.comboBox2.Location = new System.Drawing.Point(116, 81);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(115, 23);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Тип лабиринта:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(6, 689);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(231, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(6, 619);
+            this.button1.Location = new System.Drawing.Point(6, 617);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(231, 67);
             this.button1.TabIndex = 4;
@@ -94,10 +137,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(252, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(292, 49);
             this.pictureBox1.MinimumSize = new System.Drawing.Size(100, 100);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(718, 718);
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -126,5 +169,8 @@
         private GroupBox groupBox1;
         private Button button1;
         private PictureBox pictureBox1;
+        private Button button2;
+        private ComboBox comboBox2;
+        private Label label2;
     }
 }
--- Labirint/Form1.cs
+++ Labirint/Form1.cs
@@ -1,4 +1,10 @@
+using Labirint;
+using System.Diagnostics;
+using System.Drawing;
 using System.Drawing.Text;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Windows.Forms;
 
 namespace Labirint
 {
@@ -8,7 +14,6 @@ namespace Labirint
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -16,12 +21,225 @@ namespace Labirint
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Graphics g;
-            Bitmap buf;
-            buf = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            g = Graphics.FromImage(buf);
+            /*pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen pen = new Pen(Color.Aqua, 5f);
+            g.DrawRectangle(pen, 0, 0, pictureBox1.Width - pen.Width / 2, pictureBox1.Height - pen.Width / 2);
+            pictureBox1.Update();
+            pictureBox1.Image.Save("testimg.png", System.Drawing.Imaging.ImageFormat.Png);*/
+            int comboBoxSel = Convert.ToInt32(comboBox1.SelectedItem);
+            PerfectLabirint map = new PerfectLabirint(comboBoxSel,comboBoxSel,10, pictureBox1,false,true);
+            map.Generate();
+            map.Draw();
+            map.Save();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
 
+        }
+    }
+
+    class CellMapPicture
+    {
+        protected struct Cell
+        {
+            public int LocalX { get; }
+            public int LocalY { get; }
+            public Color color { get; set; }
+            public Cell(int LocalX, int LocalY, Color color)
+            {
+                this.LocalX = LocalX;
+                this.LocalY = LocalY;
+                this.color = color;
+            }
+        }
+        PictureBox pictureBox;
+        protected int heightCellMap;
+        protected int widthCellMap;
+        protected int heightPixelMap;
+        protected int widthPixelMap;
+        protected Cell[,] Cells;
+        public CellMapPicture(int heightCellMap, int widthCellMap, int scale, PictureBox pictureBox)
+        {
+            this.heightCellMap = heightCellMap;
+            this.widthCellMap = widthCellMap;
+            this.Cells = new Cell[this.widthCellMap, this.heightCellMap];
+            this.pictureBox = pictureBox;
+            this.heightPixelMap = this.heightCellMap * scale;
+            this.widthPixelMap = this.widthCellMap * scale;
+        }
+
+        public virtual void Generate()
+        {
+            for (int i = 0; i < widthCellMap; i++)
+            {
+                for (int j = 0; j < heightCellMap; j++)
+                {
+                    Cells[i, j] = new Cell(i, j, i % 2 + j % 2 == 1 ? Color.Black : Color.White);
+                }
+            }
+        }
+        public void Draw()
+        {
+            pictureBox.Image = new Bitmap(widthPixelMap,heightPixelMap);
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            Pen pen = new Pen(Color.Aqua, 5f);
+            g.FillRectangle(new SolidBrush(Color.Black), 0, 0, widthPixelMap, heightPixelMap);
+            for (int i = 0; i < widthCellMap; i++)
+            {
+                for (int j = 0; j < heightCellMap; j++)
+                {
+                    g.FillRectangle(new SolidBrush(Cells[i,j].color),
+                        (int)((widthPixelMap / (float)widthCellMap) * Cells[i, j].LocalX),
+                        (int)((heightPixelMap / (float)heightCellMap) * Cells[i, j].LocalY),
+                        (int)(widthPixelMap / (float)widthCellMap),
+                        (int)(heightPixelMap / (float)heightCellMap));
+                }
+            }
+            pictureBox.Update();
+        }
+        public void Save()
+        {
+            pictureBox.Image.Save("testimg.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+    }
+    class LabirintBase : CellMapPicture
+    {
+        protected bool displaySpecialDots;
+        protected Cell startCell;
+        protected Cell endCell;
+        public LabirintBase(int heightCellMap, int widthCellMap, int scale, PictureBox pictureBox, bool displaySpecialDots) 
+        : base((heightCellMap * 2 + 1), (widthCellMap * 2 + 1), scale, pictureBox)
+        {
+            this.displaySpecialDots = displaySpecialDots;
+        }
+        public override void Generate()
+        {
+            for (int i = 0; i < widthCellMap; i++)
+            {
+                for (int j = 0; j < heightCellMap; j++)
+                {
+                    Cells[i, j] = new Cell(i, j, i % 2 + j % 2 == 2 ?  Color.White : Color.Black);
+                }
+            }
+            if(displaySpecialDots)
+            {
+                Random rnd = new Random();
+                int[] rndDot = { rnd.Next(widthCellMap), rnd.Next(heightCellMap) };
+                while (Cells[rndDot[0], rndDot[1]].color != Color.White)
+                {
+                    rndDot[0] = rnd.Next(widthCellMap);
+                    rndDot[1] = rnd.Next(heightCellMap);
+                }
+                Cells[rndDot[0], rndDot[1]].color = Color.Green;
+                startCell = Cells[rndDot[0], rndDot[1]];
+                while (Cells[rndDot[0], rndDot[1]].color != Color.White)
+                {
+                    rndDot[0] = rnd.Next(widthCellMap);
+                    rndDot[1] = rnd.Next(heightCellMap);
+                }
+                Cells[rndDot[0], rndDot[1]].color = Color.Red;
+                endCell = Cells[rndDot[0], rndDot[1]];
+            }
+            else
+            {
+                startCell = Cells[1, 1];
+                endCell = Cells[widthCellMap - 2, heightCellMap - 2];
+            }
+        }
+    }
+}
+class RandomLabirint :LabirintBase
+{
+
+    public RandomLabirint(int heightCellMap, int widthCellMap, int scale, PictureBox pictureBox, bool displaySpecialDots)
+    : base(heightCellMap, widthCellMap, scale, pictureBox,displaySpecialDots)
+    {
+    }
+    public override void Generate()
+    {
+        base.Generate();
+        Random rnd = new Random();
+        for (int i = 1; i < widthCellMap-1; i++)
+        {
+            for (int j = 1 + i % 2; j < heightCellMap-1; j+=2)
+            {
+                Cells[i, j] = new Cell(i, j, rnd.Next(2) == 1 ? Color.White : Color.Black);
+            }
+        }
+    }
+}
+class PerfectLabirint : LabirintBase
+{
+    bool displayWay;
+    public PerfectLabirint(int heightCellMap, int widthCellMap, int scale, PictureBox pictureBox, bool displaySpecialDots, bool displayWay)
+: base(heightCellMap, widthCellMap, scale, pictureBox, displaySpecialDots)
+    {
+        this.displayWay = displayWay;
+    }
+    public override void Generate()
+    {
+        base.Generate();
+        Stack<Cell> cells = new Stack<Cell>();
+        bool [,] itsVisited = new bool[widthCellMap, heightCellMap];
+        int numbVisited = 0;
+        Random rnd = new Random();
+        cells.Push(startCell);
+        itsVisited[cells.Peek().LocalX, cells.Peek().LocalY] = true;
+        numbVisited++;
+        while (((widthCellMap - 1) / 2) * ((heightCellMap - 1) / 2) > numbVisited) 
+        {  
+            List<Cell> cellForRandomPick = new List<Cell>();
+            if (cells.Peek().LocalX < widthCellMap - 2 && !itsVisited[cells.Peek().LocalX + 2, cells.Peek().LocalY]) 
+            {
+                cellForRandomPick.Add(Cells[cells.Peek().LocalX + 2, cells.Peek().LocalY]);
+            }
+            if (cells.Peek().LocalY < heightCellMap - 2 && !itsVisited[cells.Peek().LocalX, cells.Peek().LocalY + 2]) 
+            {
+                cellForRandomPick.Add(Cells[cells.Peek().LocalX, cells.Peek().LocalY + 2]);
+            }
+            if (cells.Peek().LocalX  > 1 && !itsVisited[cells.Peek().LocalX - 2, cells.Peek().LocalY]) 
+            {
+                cellForRandomPick.Add(Cells[cells.Peek().LocalX - 2, cells.Peek().LocalY]);
+            }
+            if (cells.Peek().LocalY  > 1 && !itsVisited[cells.Peek().LocalX, cells.Peek().LocalY - 2]) 
+            {
+                cellForRandomPick.Add(Cells[cells.Peek().LocalX, cells.Peek().LocalY - 2]);
+            }
+            if(displayWay)
+            {
+                if (endCell.Equals(cells.Peek()))
+                {
+                    List<Cell> cellsCopy = new List<Cell>();
+                    cellsCopy = cells.ToList<Cell>(); 
+                    for (int i = 0; i < cells.Count(); i++) 
+                    {
+                        Cells[cellsCopy[i].LocalX, cellsCopy[i].LocalY].color = Color.LightBlue;
+                        if(i!=0)
+                        {
+                            Cells[(cellsCopy[i].LocalX+ cellsCopy[i-1].LocalX)/2, (cellsCopy[i-1].LocalY + cellsCopy[i].LocalY) / 2].color = Color.LightBlue;
+                        }
+                    }
+                    Cells[startCell.LocalX,startCell.LocalY].color = Color.Green;
+                    Cells[endCell.LocalX, endCell.LocalY].color = Color.Red;
+                }
+            }
+            if (cellForRandomPick.Count != 0)
+            {
+
+                Cell pickedCell = cellForRandomPick[rnd.Next(cellForRandomPick.Count)];
+                Cells[(cells.Peek().LocalX + pickedCell.LocalX) / 2, (cells.Peek().LocalY + pickedCell.LocalY) / 2].color = Color.White;
+                cells.Push(pickedCell);
+                numbVisited++;
+                itsVisited[cells.Peek().LocalX, cells.Peek().LocalY] = true;
+
+            }
+            else
+            {
+                cells.Pop();
+            }      
         }
     }
 }
